1e1c9b4cd9e2c355c0d8bbfbef1b0d8e2bf30840
Irina Barchuk <iabarchuk@edu.hse.ru>
2021-06-05 15:31:20 +0300

    Added filtered hists

--- src/length_hists.R
+++ src/length_hists.R
@@ -13,9 +13,9 @@ package.check('tibble')
 
 ###
 
-# NAME <- 'ENCFF881GRS.hg19'
+NAME <- 'ENCFF881GRS.hg19'
 # NAME <- 'ENCFF881GRS.hg38'
-# NAME <- '.ENCFF883IEF.hg19'
+# NAME <- 'ENCFF883IEF.hg19'
 # NAME <- 'ENCFF883IEF.hg38'
 
 OUT_DIR <- '../results/'
@@ -26,22 +26,10 @@ bed_df <- read.delim(paste0('../data/', NAME, '.bed'), as.is = TRUE, header = FA
 colnames(bed_df) <- c('chrom', 'start', 'end')
 bed_df$len <- bed_df$end - bed_df$start
 
-head(bed_df)
-
-bed_df <- bed_df %>%
-    arrange(-len) %>%
-    filter(len < 50000)
-
 ggplot(bed_df) +
     aes(x = len) +
     geom_histogram() +
     ggtitle(NAME, subtitle = sprintf('Number of peaks = %s', nrow(bed_df))) +
     theme_bw()
-ggsave(paste0('len_hist.', NAME, '.pdf'), path = OUT_DIR)
 
-#
-#bed_df %>%
-#    select(-len) %>%
-#    write.table(file='data/H3K4me3_A549.ENCFF832EOL.hg19.filtered.bed',
-#                col.names = FALSE, row.names = FALSE, sep = '\t', quote = FALSE)
-#
+ggsave(paste0('len_hist.', NAME, '.pdf'), path = OUT_DIR)
