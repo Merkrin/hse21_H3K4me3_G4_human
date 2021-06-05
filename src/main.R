7b925b41d5d361525700240acdcbd5205adfd678
Irina Barchuk <iabarchuk@edu.hse.ru>
2021-06-05 15:13:15 +0300

    Initial project commit

--- src/main.R
+++ src/main.R
@@ -13,17 +13,17 @@ package.check('tibble')
 
 ###
 
-# NAME <- 'H3K4me3_A549.ENCFF881GRS.hg19'
-# NAME <- 'H3K4me3_A549.ENCFF881GRS.hg38'
-# NAME <- 'H3K4me3_A549.ENCFF883IEF.hg19'
-NAME <- 'H3K4me3_A549.ENCFF883IEF.hg38'
+# NAME <- 'ENCFF881GRS.hg19'
+# NAME <- 'ENCFF881GRS.hg38'
+# NAME <- '.ENCFF883IEF.hg19'
+# NAME <- 'ENCFF883IEF.hg38'
 
-OUT_DIR <- 'Results/'
+OUT_DIR <- '../results/'
 
 ###
 
-bed_df <- read.delim(paste0('data/', NAME, '.bed'), as.is = TRUE, header = FALSE)
-colnames(bed_df) <- c('chrom', 'start', 'end', 'name', 'score')
+bed_df <- read.delim(paste0('../data/', NAME, '.bed'), as.is = TRUE, header = FALSE)
+colnames(bed_df) <- c('chrom', 'start', 'end')
 bed_df$len <- bed_df$end - bed_df$start
 
 head(bed_df)
@@ -39,7 +39,9 @@ ggplot(bed_df) +
     theme_bw()
 ggsave(paste0('len_hist.', NAME, '.pdf'), path = OUT_DIR)
 
-bed_df %>%
-    select(-len) %>%
-    write.table(file='data/H3K4me3_A549.ENCFF832EOL.hg19.filtered.bed',
-                col.names = FALSE, row.names = FALSE, sep = '\t', quote = FALSE)
+#
+#bed_df %>%
+#    select(-len) %>%
+#    write.table(file='data/H3K4me3_A549.ENCFF832EOL.hg19.filtered.bed',
+#                col.names = FALSE, row.names = FALSE, sep = '\t', quote = FALSE)
+#
